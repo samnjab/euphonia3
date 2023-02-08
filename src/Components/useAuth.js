0ee48fb3e64f6c470813b923e1ee897790174b36
samjaberi <samanjaberi.sj@gmail.com>
2023-02-08 13:17:58 -0500

    changed server folder to api folder and editted the endpoints in useAuth to match the serverless function endpoints

--- src/Components/useAuth.js
+++ src/Components/useAuth.js
@@ -8,7 +8,7 @@ export default function useAuth(code) {
 
   useEffect(() => {
     axios
-      .post("http://localhost:3001/login", {
+      .post("http://localhost:3001/api/server/login", {
         code,
       })
       .then(res => {
@@ -26,7 +26,7 @@ export default function useAuth(code) {
     if (!refreshToken || !expiresIn) return
     const interval = setInterval(() => {
       axios
-        .post("http://localhost:3001/refresh", {
+        .post("http://localhost:3001/api/server/refresh", {
           refreshToken,
         })
         .then(res => {
