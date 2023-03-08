b59b87c2d7370d6bfe80761ee5ecc4069fb71c51
samjaberi <samanjaberi.sj@gmail.com>
2023-03-08 15:11:12 -0500

    added mouseover and mouseleave events to recotrack

--- src/Components/RecoTrack.js
+++ src/Components/RecoTrack.js
@@ -1,56 +1,31 @@
 import React from "react"
-import {useState, useEffect} from 'react'
+import {useState, useEffect, useRef} from 'react'
 
 import Playlists from './Playlists';
 import SoloView from "./SoloView";
 
 export default function TrackSearchResult({ track, preview_url, playTrack, selectTrack, spotifyApi, user, playingStatus, playingTrack, changePlay }) {
     const [viewSolo, setViewSolo] = useState(false)
-    console.log('track is', track)
-    
-
+    const keepPlaying = useRef(false)
     return ( 
       <div className='recoTrack'>
           <audio src={preview_url} id={`${track.uri}`}></audio>
-          <div onClick={() => {
-                let audioElement = document.getElementById(`${track.uri}`)
-                console.log(audioElement)
-                audioElement.play()
-              } }>Play track preview</div>
-            <div ></div>
-            <div 
-            className='iconBox'>
-                {   playingTrack.id !== track.id ?
-                    <i className="fa-solid fa-play play"
-                        onClick= {()=> {
-                            playTrack(track)
-                            changePlay(true)
-                            }}>
-                    </i>    
-                    :
-                    (
-                        playingStatus ?
-                        <i className="fa-solid fa-pause pause" 
-                            onClick={()=>{
-                                changePlay(false)
-                            }}>
-                        </i>
-                        :
-                        <i className="fa-solid fa-play play" 
-                        onClick={()=>{
-                            changePlay(true)
-                            }}>   
-                        </i>
-                    )
-                }
-            </div>
-            {console.log('preview url', preview_url)}
-            <a src={preview_url} 
-            onClick={() => setViewSolo(true) }
-            onHover ={()=>{
+            <a  
+            onClick={() => {
+                setViewSolo(true)
+                keepPlaying.current = true
+            } }
+            onMouseEnter ={() =>{
                 const audioElement = document.getElementById(`${track.uri}`)
                 console.log(audioElement)
                 audioElement.play()
+            }}
+            onMouseLeave={() => {
+                if (!keepPlaying){
+                    const audioElement = document.getElementById(`${track.uri}`)
+                    console.log(audioElement)
+                    audioElement.pause()
+                }
             }}
             >
                 <img src={track.albumUrl} className='cover' />
